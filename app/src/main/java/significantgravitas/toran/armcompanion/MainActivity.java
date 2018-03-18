748941ebb66d149d6c81fb16f8ad446f2b0a102d
Torantulino <40276179@live.napier.ac.uk>
2018-03-18 03:38:10 +0000

    Fragments working, Now designing each. Starting Redesign of Save Frag.

--- app/src/main/java/significantgravitas/toran/armcompanion/MainActivity.java
+++ app/src/main/java/significantgravitas/toran/armcompanion/MainActivity.java
@@ -1,6 +1,8 @@
 package significantgravitas.toran.armcompanion;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
@@ -8,8 +10,15 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements  DressUpFragment.OnFragmentInteractionListener,
+                                                                SaveOutfitFragment.OnFragmentInteractionListener,
+                                                                PurchaseOutfitFragment.OnFragmentInteractionListener,
+                                                                MyWardrobeFragment.OnFragmentInteractionListener,
+                                                                MeFragment.OnFragmentInteractionListener{
 
     // Global Variables
     TextView mainTitle;
@@ -22,9 +31,22 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        // - Load "Dress up" as startup page -
+        // set title
         mainTitle = (TextView)findViewById(R.id.txtAppTitle);
         mainTitle.setText(R.string.nav_dress_up);
+        // load fragment
+        Fragment fragment = new DressUpFragment();
+        try {
+            loadFragment(fragment);
+        }
+        catch (Exception ex)
+        {
+        Log.e("FragLoadError", ex.getMessage());
+        }
     }
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new
             BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,10 +56,18 @@ public class MainActivity extends AppCompatActivity {
                     Fragment mainFragment;
                     switch (item.getItemId()) {
                         case R.id.navigation_dress_up:
+                            // Set title
                             mainTitle.setText(R.string.nav_dress_up);
+                            // Load fragment
+                            mainFragment = new DressUpFragment();
+                            loadFragment(mainFragment);
                             return true;
                         case R.id.navigation_save_outfit:
+                            // Set title
                             mainTitle.setText(R.string.nav_outfit_save);
+                            // Load fragment
+                            mainFragment = new SaveOutfitFragment();
+                            loadFragment(mainFragment);
                             return true;
                         case R.id.navigation_purchase_outfit:
                             mainTitle.setText(R.string.nav_outfit_purchase);
@@ -53,4 +83,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragHolder, fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    /*
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragHolder, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();*/
 }
+
